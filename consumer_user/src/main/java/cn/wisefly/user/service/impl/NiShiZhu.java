44cbb660c9033ffd351ad56ec94460208834f885
zhixiang0222 <zhixiang0222@163.com>
2021-09-16 19:45:30 +0800

    有惊喜

---
+++ consumer_user/src/main/java/cn/wisefly/user/service/impl/NiShiZhu.java
@@ -0,0 +1,16 @@
+package cn.wisefly.user.service.impl;
+
+/**
+ * @Author muzhixiang
+ * @Date 2021/9/16 19:43
+ * @Description
+ */
+public class NiShiZhu {
+
+  public static void main(String[] args) {
+
+    System.out.println("你是猪");
+
+  }
+
+}
